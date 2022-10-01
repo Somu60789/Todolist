35ff78fda6b8429a5a5b9fb4ad3494b4e48a840d
Somasekhar Eruvuri <ess527162@tatamotors.com>
2022-10-01 21:12:02 +0530

    final

--- src/main/java/com/todolist/todolist/controller/tasksController.java
+++ src/main/java/com/todolist/todolist/controller/tasksController.java
@@ -10,7 +10,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
@@ -27,49 +29,39 @@ public class tasksController {
         return this.tasksRepository.findAll();
     }
     //get task by id
-    @GetMapping("/tasks/{id}")
-    public ResponseEntity<tasks> getTaskById(@PathVariable(value = "id") Long employeeId)
+    @GetMapping("tasks/{id}")
+    public ResponseEntity<tasks> getTaskById(@PathVariable(value = "id") Long id)
             throws ResourceNotFoundException {
-        tasks tasks = tasksRepository.findById(Long  id)
+        tasks tasks = tasksRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Task not found for this id :: " + id));
         return ResponseEntity.ok().body(tasks);
     }
     //save task
-    @PostMapping("/tasks")
+    @PostMapping("tasks")
     public tasks createTasks(@Validated @RequestBody tasks tasks){
+
         return  tasksRepository.save(tasks);
     }
     //update task
-    @PutMapping("/tasks/{id}")
+    @PutMapping("tasks/{id}")
     public ResponseEntity<tasks> updateTasks(@PathVariable(value = "id") Long id,
-                                             @Validated @RequestBody tasks title) throws ResourceNotFoundException{
-        tasks tasks = tasksRepository.findAllById(id)
+                                             @Validated @RequestBody tasks taskDetails) throws ResourceNotFoundException{
+        tasks tasks = tasksRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("task not found for this id :: "+ id));
-        tasks.setTitle(tasks);
+        tasks.setTitle(taskDetails.getTitle());
+        tasks.setId(taskDetails.getId());
+        tasks.setCompleted(taskDetails.getCompleted());
+
+        return ResponseEntity.ok(this.tasksRepository.save(tasks));
     }
     //delete task
-}
-    @PutMapping("/employees/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
-                                                   @Valid @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
-        Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
-        employee.setEmailId(employeeDetails.getEmailId());
-        employee.setLastName(employeeDetails.getLastName());
-        employee.setFirstName(employeeDetails.getFirstName());
-        final Employee updatedEmployee = employeeRepository.save(employee);
-        return ResponseEntity.ok(updatedEmployee);
-    }
-
-    @DeleteMapping("/employees/{id}")
-    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
-            throws ResourceNotFoundException {
-        Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
-        employeeRepository.delete(employee);
+    @DeleteMapping("tasks/{id}")
+    public Map<String, Boolean> deleteTasks(@PathVariable(value = "id") Long id) throws ResourceNotFoundException{
+        tasks tasks = tasksRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found for this id :: " + id));
+        this.tasksRepository.delete(tasks);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
     }
+}
